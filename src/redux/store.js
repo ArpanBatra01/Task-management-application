ddab072607080c92d79725893f9ffb56335bddb0
ArpanBatra01 <arpanbatra60@gmail.com>
2024-12-16 18:58:11 +0530

    feat/project-setup

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import tasksReducer from './tasks/taskSlice';
+
+
+const store = configureStore({
+
+    reducer: {
+        tasks: tasksReducer,
+    }
+
+});
+
+export default store
---
+++ src/redux/tasks/taskSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    tasks: [],
+    filter: 'ALL',
+};
+
+
+const taskSlice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {
+        addTask: (state, action) => {
+            state.tasks.push(action.payload);
+        },
+
+        toggleTask: (state, action) => {
+            const task = state.tasks.find((task) => task.id === action.payload);
+            if (task) {
+                task.completed = !task.completed;
+            }
+        },
+
+        removeTask: (state, action) => {
+            state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+
+        },
+
+        editTask: (state, action) => {
+            const { id, name, description, dueDate } = action.payload;
+            const task = state.tasks.find((task) => task.id === id);
+
+            if (task) {
+                task.name = name;
+                task.description = description;
+                task.dueDate = dueDate;
+            }
+        },
+
+        setFilter: (state, action) => {
+            state.filter = action.payload;
+        },
+    },
+
+});
+
+export const { addTask, toggleTask, removeTask, editTask, setFilter } = taskSlice.actions;
+
+export default taskSlice.reducer;
